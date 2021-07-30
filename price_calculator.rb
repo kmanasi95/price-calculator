730058b97fa26b6ea8574e0c47490577ce126edc
Manasi Kulkarni <manasikulkarni@Manasis-MBP.lan>
2021-07-30 15:13:30 -0400

    Variable names

--- price_calculator.rb
+++ price_calculator.rb
@@ -1,8 +1,6 @@
 #!/usr/bin/ruby -w
 
 class PriceCalculator
-    attr_accessor :items
-
     UNIT_PRICES = {
         'Milk' => 3.97,
         'Bread' => 2.17,
@@ -15,29 +13,27 @@ class PriceCalculator
         'Bread' => {'quantity' => 3, 'amount' => 6.00}
     }
 
-    Total = {
-    }
-
     def initialize(items)
         @items = items.split(',').map(&:strip).map(&:capitalize)
+        @total_purchases = Hash.new
     end
 
     def calculate
         @items.each do |item|
             if SALE_PRICES.key?(item) 
-                if Total.key(item)
+                if @total_purchases.key(item)
                     next
                 else
-                    q, r = @items.count(item).divmod(SALE_PRICES[item]['quantity'])
-                    amount = (q * SALE_PRICES[item]['amount']) + (r * UNIT_PRICES[item])
-                    Total[item] = {'quantity' => @items.count(item), 'amount' => amount}
+                    quotient, remainder = @items.count(item).divmod(SALE_PRICES[item]['quantity'])
+                    amount = (quotient * SALE_PRICES[item]['amount']) + (remainder * UNIT_PRICES[item])
+                    @total_purchases[item] = {'quantity' => @items.count(item), 'amount' => amount}
                 end
             elsif UNIT_PRICES.key?(item)
-                if Total.key(item)
+                if @total_purchases.key(item)
                     next
                 else
                     amount = @items.count(item) * UNIT_PRICES[item]
-                    Total[item] = {'quantity' => @items.count(item), 'amount' => amount}
+                    @total_purchases[item] = {'quantity' => @items.count(item), 'amount' => amount}
                 end
             end
         end
@@ -48,23 +44,26 @@ class PriceCalculator
         puts "\n"
         puts headers.join('       ')
         puts '---------------------------------'
-        Total.each { |k, v| puts "#{k.ljust(13)} #{v['quantity'].to_s.ljust(12)} #{'$' + v['amount'].to_s}" }
+        @total_purchases.each { |k, v| puts "#{k.ljust(13)} #{v['quantity'].to_s.ljust(12)} #{'$' + v['amount'].to_s}" }
 
-        price = 0
-        total = 0
+        current_price = 0
+        actual_price = 0
 
         @items.each do |item|
-            total += UNIT_PRICES[item]
+            if UNIT_PRICES.key?(item)
+                actual_price += UNIT_PRICES[item]
+            end
         end
 
-        Total.each do |k, v|
-            price += v['amount']
+        @total_purchases.each do |k, v|
+            current_price += v['amount']
         end
 
-        savings = (total - price).round(2)
+        current_price = current_price.round(2)
+        savings = (actual_price - current_price).round(2)
 
         puts "\n"
-        puts "Total price : #{'$' + price.to_s}"
+        puts "Total price : #{'$' + current_price.to_s}"
         puts "You saved #{'$' + savings.to_s} today."
         puts "\n"
     end
