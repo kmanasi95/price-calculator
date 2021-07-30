6d33b5bc60a60103d7f249cb851db915e8b2a90e
Manasi Kulkarni <manasikulkarni@Manasis-MBP.lan>
2021-07-30 12:54:56 -0400

    Display as a table

--- price_calculator.rb
+++ price_calculator.rb
@@ -4,47 +4,69 @@ class PriceCalculator
     attr_accessor :items
 
     UNIT_PRICES = {
-        'milk' => 3.97,
-        'bread' => 2.17,
-        'banana' => 0.99,
-        'apple' => 0.89
+        'Milk' => 3.97,
+        'Bread' => 2.17,
+        'Banana' => 0.99,
+        'Apple' => 0.89
     }
 
     SALE_PRICES = {
-        'milk' => {'quantity' => 2, 'amount' => 5.00},
-        'bread' => {'quantity' => 3, 'amount' => 6.00}
+        'Milk' => {'quantity' => 2, 'amount' => 5.00},
+        'Bread' => {'quantity' => 3, 'amount' => 6.00}
     }
 
     Total = {
     }
 
     def initialize(items)
-        @items = items.split(',')
-        @Total = Total
+        @items = items.split(',').map(&:strip).map(&:capitalize)
     end
 
     def calculate
-
         @items.each do |item|
-            if SALE_PRICES.key?(item.strip.downcase) 
-                if @Total.key(item)
+            if SALE_PRICES.key?(item) 
+                if Total.key(item)
                     next
                 else
-                    q, r = @items.count(item.strip).divmod(SALE_PRICES[item.strip.downcase]['quantity'])
-                    amount = (q * SALE_PRICES[item.strip.downcase]['amount']) + (r * UNIT_PRICES[item.strip.downcase])
-                    @Total[item] = amount
+                    q, r = @items.count(item).divmod(SALE_PRICES[item]['quantity'])
+                    amount = (q * SALE_PRICES[item]['amount']) + (r * UNIT_PRICES[item])
+                    Total[item] = {'quantity' => @items.count(item), 'amount' => amount}
                 end
-            elsif UNIT_PRICES.key?(item.strip.downcase)
-                if @Total.key(item)
+            elsif UNIT_PRICES.key?(item)
+                if Total.key(item)
                     next
                 else
-                    amount = @items.count(item.strip) * UNIT_PRICES[item.strip.downcase]
-                    @Total[item] = amount
+                    amount = @items.count(item) * UNIT_PRICES[item]
+                    Total[item] = {'quantity' => @items.count(item), 'amount' => amount}
                 end
             end
         end
+    end
 
-        puts @Total
+    def display()
+        headers = ['Item', 'Quantity', 'Amount']
+        puts "\n"
+        puts headers.join('       ')
+        puts '---------------------------------'
+        Total.each { |k, v| puts "#{k.ljust(13)} #{v['quantity'].to_s.ljust(12)} #{'$' + v['amount'].to_s}" }
+
+        price = 0
+        total = 0
+
+        @items.each do |item|
+            total += UNIT_PRICES[item]
+        end
+
+        Total.each do |k, v|
+            price += v['amount']
+        end
+
+        savings = (total - price).round(2)
+
+        puts "\n"
+        puts "Total price : #{'$' + price.to_s}"
+        puts "You saved #{'$' + savings.to_s} today."
+        puts "\n"
     end
 
 end
@@ -53,4 +75,5 @@ puts 'Please enter all the items purchased separated by a comma: '
 items = gets.chomp
 
 obj = PriceCalculator.new(items)
-puts obj.calculate
+obj.calculate
+obj.display
